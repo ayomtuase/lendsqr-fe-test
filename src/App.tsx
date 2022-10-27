7756d3a70fa8316046f49bb07a56c2117374c49c
Ayomide Oguntuase <oguntuase3@gmail.com>
2022-10-27 23:37:10 +0100

    lazy loaded components and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,49 @@
 import "./App.scss";
-import Login from "./pages/Login";
-import Users from "./pages/Dashboard/Users";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import SingleUser from "./pages/Dashboard/SingleUser";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Loader from "./components/Loader";
 import ErrorPage from "./pages/ErrorPage";
 import NotFound from "./pages/NotFound";
+import { lazy, Suspense } from "react";
+
+const Login = lazy(() => import("./pages/Login"));
+const Users = lazy(() => import("./pages/Dashboard/Users"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const SingleUser = lazy(() => import("./pages/Dashboard/SingleUser"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />,
+    element: (
+      <Suspense fallback={<Loader/>}>
+        <Login />
+      </Suspense>
+    ),
     errorElement: <ErrorPage />,
   },
   {
     path: "/dashboard",
-    element: <Dashboard />,
+    element: (
+      <Suspense fallback={<Loader/>}>
+        <Dashboard />
+      </Suspense>
+    ),
     children: [
       {
         index: true,
-        element: <Users />,
+        element: (
+          <Suspense fallback={<Loader/>}>
+            <Users />
+          </Suspense>
+        ),
       },
       {
         path: "/dashboard/users/:id",
-        element: <SingleUser />,
+        element: (
+          <Suspense fallback={<Loader/>}>
+            <SingleUser />
+          </Suspense>
+        ),
       },
     ],
   },
